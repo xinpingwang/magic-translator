026531283e81d080ce6f276d7dd50c34846d1d9b
XinPing Wang <xinping.wang@hotmail.com>
2019-11-25 15:41:07 +0800

    init project

---
+++ Magic Translator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Magic Translator
+//
+//  Created by Arestolus Wang on 25/11/2019.
+//  Copyright © 2019 Magic Done. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Magic Translator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic Translator
+//
+//  Created by Arestolus Wang on 25/11/2019.
+//  Copyright © 2019 Magic Done. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
